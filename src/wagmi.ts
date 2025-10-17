b309e8cdd14fa7153d9d837cb5f51c1df6aec101
0xward <aradeawardana97@gmail.com>
2025-10-17 16:10:54 +0700

    Update wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,16 +1,9 @@
 'use client';
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
-import {
-  coinbaseWallet,
-  metaMaskWallet,
-  rainbowWallet,
-} from '@rainbow-me/rainbowkit/wallets';
 import { useMemo } from 'react';
 import { http, createConfig } from 'wagmi';
 import { base, baseSepolia } from 'wagmi/chains';
 import { NEXT_PUBLIC_WC_PROJECT_ID } from './config';
 
-// LANGKAH 1: Import konektor khusus Farcaster
 import { farcasterFrame } from '@farcaster/frame-wagmi-connector'; 
 
 export function useWagmiConfig() {
@@ -22,42 +15,15 @@ export function useWagmiConfig() {
   }
 
   return useMemo(() => {
-    // Definisi konektor Farcaster
     const farcasterConnector = farcasterFrame();
     
-    // LANGKAH 2: Tambahkan konektor Farcaster ke daftar dompet
-    const connectors = connectorsForWallets(
-      [
-        {
-          groupName: 'Recommended Wallet',
-          wallets: [
-            // Tambahkan Farcaster Connector sebagai opsi utama di sini
-            ({ chains }) => ({
-                id: farcasterConnector.id,
-                name: farcasterConnector.name,
-                iconUrl: farcasterConnector.icon.light,
-                iconBackground: farcasterConnector.iconBackground,
-                createConnector: farcasterConnector.createConnector,
-            }),
-            coinbaseWallet, // Dompet yang sudah ada
-          ],
-        },
-        {
-          groupName: 'Other Wallets',
-          wallets: [rainbowWallet, metaMaskWallet],
-        },
-      ],
-      {
-        appName: 'onchainkit',
-        projectId,
-      },
-    );
+    const connectors = [farcasterConnector];
 
     const wagmiConfig = createConfig({
       chains: [base, baseSepolia],
-      // turn off injected provider discovery
-      multiInjectedProviderDiscovery: false,
-      connectors,
+      autoConnect: true, 
+      multiInjectedProviderDiscovery: false, 
+      connectors, 
       ssr: true,
       transports: {
         [base.id]: http(),
